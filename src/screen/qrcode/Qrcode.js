7128db1243ba05dce708fe0e1dfbc80d1d5b4ee7
Maxence <maxence.lauque@epitech.eu>
2019-09-10 23:38:59 +0200

    mise à jour package QRCODE

--- src/screen/qrcode/Qrcode.js
+++ src/screen/qrcode/Qrcode.js
@@ -1,76 +1,118 @@
 import React, { Component } from 'react';
-import { Alert, Linking, Dimensions, LayoutAnimation, Text, View, StyleSheet, TouchableOpacity } from 'react-native';
-import { BarCodeScanner, Permissions } from 'expo';
+import { Alert, Linking, Dimensions, LayoutAnimation, Text, View, StyleSheet, TouchableOpacity, Button } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 import Header from './../../global/header/Header';
 import Store from './../../global/store/Store'
 
 export default class App extends Component {
-  state = {
-    hasCameraPermission: null,
-    lastScannedUrl: null,
-  };
 
-  componentDidMount() {
-    this._requestCameraPermission();
-  }
 
-  _requestCameraPermission = async () => {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({
-      hasCameraPermission: status === 'granted',
-    });
-  };
 
-  _handleBarCodeRead = result => {
-    fetch('http://51.38.187.216:9090/cnxTable2/useToken', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        tokenTable: result.data,
-        token: Store.Token,
-      }),
-    }).then((response) => response.json())
-        .then((responseJson) => {
-          if (responseJson.status == 200) {
-	           this.props.navigation.navigate('Profil')
-             Alert.alert('Table connectée')
-          }
-          else {
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-  };
 
-  render() {
-    return (
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    state = {
+      hasCameraPermission: null,
+      scanned: false,
+    };
+
+    async componentDidMount() {
+      this.getPermissionsAsync();
+    }
+
+    getPermissionsAsync = async () => {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA);
+      this.setState({ hasCameraPermission: status === 'granted' });
+    };
+
+    render() {
+      const { hasCameraPermission, scanned } = this.state;
+
+      if (hasCameraPermission === null) {
+        return <Text>Requesting for camera permission</Text>;
+      }
+      if (hasCameraPermission === false) {
+        return <Text>No access to camera</Text>;
+      }
+      return (
+
+
+
       <View style={styles.container}>
         <Header navigation={this.props.navigation}/>
 
-        {this.state.hasCameraPermission === null
-          ? <Text>Requesting for camera permission</Text>
-          : this.state.hasCameraPermission === false
-              ? <Text style={{ color: '#fff' }}>
-                  Camera permission is not granted
-                </Text>
-              : <BarCodeScanner
-                  onBarCodeRead={this._handleBarCodeRead}
-                  style={{
-                    height: Dimensions.get('window').height,
-                    width: Dimensions.get('window').width,
-                  }}
-                />}
+          <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+            style={StyleSheet.absoluteFillObject}
+          />
+
+          {scanned && (
+            <Button title={'Tap to Scan Again'} onPress={() => this.setState({ scanned: false })} />
+          )}
+
+
         <TouchableOpacity onPress={() => this.props.navigation.navigate('Dashboard')} style={styles.bottomBar}>
           <Text style={styles.cancelButtonText}>Retour</Text>
         </TouchableOpacity>
       </View>
-    );
-  }
+      );
+    }
+
+    handleBarCodeScanned = ({ type, data }) => {
+          this.setState({ scanned: true });
+          fetch('http://51.38.187.216:9090/cnxTable2/useToken', {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+              token: Store.Token,
+            },
+            body: JSON.stringify({
+              tokenTable: data,
+            }),
+          }).then((response) => response.json())
+              .then((responseJson) => {
+               Alert.alert('lol')
+
+                if (responseJson.status == 200) {
+      	           this.props.navigation.navigate('Profil')
+                   Alert.alert('Table connectée')
+                }
+                else {
+                  Alert.alert('ça marche pas')
+                }
+              })
+              .catch((error) => {
+                console.error(error);
+              });
+    };
+
+
+
+
+
+
+
+
+
+
+
 }
 
 const styles = StyleSheet.create({
